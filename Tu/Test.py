a71d01145754d89615e46555eb74122aad1b6f0b
Tran Thanh Tu <tranthanhtuxd2211@gmail.com>
2016-06-14 17:13:49 +0700

    update

--- Tu/Test.py
+++ Tu/Test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect,url_for
 app = Flask(__name__,static_url_path='')
 
 
@@ -9,23 +9,33 @@ def index():
 @app.route('/index')
 def index_return():
     return render_template("index.html")
+# -------------------------------------------------
+@app.route('/lowcarb')
+def lowcarb():
+    return render_template("lowcarb.html")
 
-@app.route('/resturants')
-def resturants():
-    return render_template("resturants.html")
+@app.route('/orders')
+def orders():
+    return render_template("orders.html")
+#--------------------------------------------------
+
+@app.route('/BMI_request<bmr>,<BMI>')
+def BMI_request(bmr,BMI):
+    return render_template('BMI_request.html',bmr=bmr,BMI=BMI)
 
 @app.route('/BMI',methods=['GET','POST'])
 def bmi():
     if request.method == 'POST':
-        print(request.form['render'])
-        if request.form['render'] == request.form['male']:
+        if request.form['gender'] == "Male":
             bmr = 66.47 + (13.75  * int(request.form['weight'])) + (5.0 * int(request.form['height'])) - (6.75 * int(request.form['age']))
-            print(bmr)
-        elif request.form['render'] == request.form['female']:
+            BMI = int(request.form['weight']) / (int(request.form['height']) / 100) ** 2
+        elif request.form['gender'] == "Female":
             bmr = 665.09 + (9.56 * int(request.form['weight'])) + (1.84 * int(request.form['height'])) - (4.67 * int(request.form['age']))
-            print(bmr)
-        return redirect('/resturants')
-    return render_template("BMI.html")
+            BMI = int(request.form['weight']) / (int(request.form['height']) / 100) ** 2
+        return redirect(url_for('BMI_request',bmr=bmr,BMI=BMI))
+    return render_template("BMI.html" )
+#--------------------------------------------------------------------
+
 
 @app.route('/register')
 def register():
@@ -34,8 +44,6 @@ def register():
 @app.route('/login',methods=['GET','POST'])#vut thuoc tinh vao day
 def login():
     if request.method == 'POST':
-        print("username is: ",request.form['username'])
-        print("password is: ",request.form['password'])
         return redirect('/')
     return render_template("login.html")
 
